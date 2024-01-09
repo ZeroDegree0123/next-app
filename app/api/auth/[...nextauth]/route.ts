88b59f2f97af7982f8b106f81443af0f4ed65de3
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2024-01-08 21:49:44 -0800

    starts 2nd authentication provider Github

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client"
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -33,7 +34,11 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!
+        }),
     ],
     session: {
         strategy: 'jwt'
